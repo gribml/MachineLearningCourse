c42f0c44e60bceca749a74d693d37837d4ed3325
sozlen <sozlennn@gmail.com>
2012-10-27 21:22:02 +0300

    trial

--- assignment1/igClassify.m
+++ assignment1/igClassify.m
@@ -3,6 +3,7 @@ function cl = igClassify(answer, depth, ig)
 % down the branches of each tree given the attributes of the example
 % answer - result array from walking down each tree
 % ig - sum of information gain array from walking down each tree
+%
     if ( length(answer(answer==1))==1 ) 
         cl = find(answer==1);
     elseif ( length(answer(answer==1)) > 1 )
