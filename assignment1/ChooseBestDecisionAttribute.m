ead8cd0f4fd22a61dc14bc40df73df138d65e520
Jean Kossaifi <jk712@visual16.doc.ic.ac.uk>
2012-10-31 13:46:19 +0000

    Few comments in ChooseBestDecisionAttribute

--- assignment1/ChooseBestDecisionAttribute.m
+++ assignment1/ChooseBestDecisionAttribute.m
@@ -1,4 +1,8 @@
 function [best_attribute, maxGain] = ChooseBestDecisionAttribute(examples, attributes, binary_targets)
+% This function returns the best attribute, ie the one with the highest
+% information gain
+% maxGain is the information gain of the selected best_attribute
+
     % definition of the entropy
     function [entropy] = I(p, n)
         if or( p == 0, n == 0 )
@@ -19,6 +23,8 @@ function [best_attribute, maxGain] = ChooseBestDecisionAttribute(examples, attri
         end
     end
     
+    % it is binary classification
+    % we accept every values, being vNo and vYes
     vNo = values(1);  % 0
     vYes = values(2);  % 1
     p = sum(binary_targets==vNo); % count of zeros in binary targets
@@ -27,7 +33,8 @@ function [best_attribute, maxGain] = ChooseBestDecisionAttribute(examples, attri
     best_attribute = attributes(1);
     maxGain = -1; % as gain is > 0, will be overwritten at first iteration
     
-    
+    % We compute the information gain for every attribute 
+    % and select the best one
     for i=1:length(attributes)
         n0 = sum(binary_targets(examples(:, i)==0)==vNo);
         p0 = sum(binary_targets(examples(:, i)==0)==vYes);
@@ -37,6 +44,9 @@ function [best_attribute, maxGain] = ChooseBestDecisionAttribute(examples, attri
         remainder = (n0 + p0)*I(p0, n0)/s + (p1 + n1)*I(p1, n1)/s;
         gain = global_entropy - remainder;
         
+        % the information gain of the current attribute is the highest one
+        % for now:
+        % We keep it
         if gain > maxGain
             best_attribute = attributes(i);
             maxGain = gain;
