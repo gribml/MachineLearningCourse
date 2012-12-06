9b60cd6b7c339bb49d06dfea2b5e243d57cfdb95
romain <romain@romain-M15x.(none)>
2012-12-06 20:47:57 +0000

    ttest2 -> ttest Deleted .mat

--- 5assignment/Tests.m
+++ 5assignment/Tests.m
@@ -2,14 +2,14 @@ Valid;
 % ValidTrain
 
 %% Parameters
-alpha = 0.05; % ttest at 5%.
+alpha = 0.05 / 3;
 clear_all = false;
 
 fprintf( '\nttest: alpha = %f\n\n', alpha );
 for i=1:6
     fprintf( '\nemotion %ld\n', i );
-    [ h1, p1, c1 ] = ttest2( NN_F( :, i ), DT_F( :, i ), alpha );
-    fprintf( '  ttest2 NN_F, DT_F: p=%f, c=[%f %f]\n', p1, c1( 1 ), c1( 2 ) );
+    [ h1, p1, c1 ] = ttest( NN_F( :, i ), DT_F( :, i ), alpha );
+    fprintf( '  ttest NN_F, DT_F: p=%f, c=[%f %f]\n', p1, c1( 1 ), c1( 2 ) );
     if ~h1
         fprintf( '    Null Hypothesis cannot be rejected, NN and DT have the same performance on emotion %ld\n', i );
     else
@@ -20,8 +20,8 @@ for i=1:6
         end
     end
     
-    [ h2, p2, c2 ] = ttest2( DT_F( :, i ), CBR_F( :, i ), alpha );
-    fprintf( '  ttest2 DT_F, CBR_F: p=%f, c=[%f %f]\n', p2, c2( 1 ), c2( 2 ) );
+    [ h2, p2, c2 ] = ttest( DT_F( :, i ), CBR_F( :, i ), alpha );
+    fprintf( '  ttest DT_F, CBR_F: p=%f, c=[%f %f]\n', p2, c2( 1 ), c2( 2 ) );
     if ~h2
         fprintf( '    Null Hypothesis cannot be rejected, DT and CBR have the same performance on emotion %ld\n', i );
     else
@@ -31,9 +31,9 @@ for i=1:6
             fprintf( '    Null Hypothesis rejected, CBR is more performant than DT on emotion %ld\n', i );
         end
     end
-    
-    [ h3, p3, c3 ] = ttest2( CBR_F( :, i ), NN_F( :, i ), alpha );
-    fprintf( '  ttest2 CBR_F, NN_F: p=%f, c=[%f %f]\n', p3, c3( 1 ), c3( 2 ) );
+
+    [ h3, p3, c3 ] = ttest( CBR_F( :, i ), NN_F( :, i ), alpha );
+    fprintf( '  ttest CBR_F, NN_F: p=%f, c=[%f %f]\n', p3, c3( 1 ), c3( 2 ) );
     if ~h3
         fprintf( '    Null Hypothesis cannot be rejected, CBR and NN have the same performance on emotion %ld\n', i );
     else
